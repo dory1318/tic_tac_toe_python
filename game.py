a5d0ed57859607da90b327cbc1afc0b0422995dd
Dora Kemeny <dory1318@gmail.com>
2019-08-06 18:43:11 +0100

    winning moves

--- game.py
+++ game.py
@@ -17,8 +17,34 @@ class Game():
         else:
             return False
 
-    def game_over(self):
+    def full_board(self):
         if not " " in self.new_board:
             return True
         else:
             return False
+
+    def x_won(self):
+        if(
+            (self.new_board[0] == "x" and self.new_board[1] == "x" and self.new_board[2] == "x") or
+            (self.new_board[3] == "x" and self.new_board[4] == "x" and self.new_board[5] == "x") or
+            (self.new_board[6] == "x" and self.new_board[7] == "x" and self.new_board[8] == "x") or
+            (self.new_board[0] == "x" and self.new_board[3] == "x" and self.new_board[6] == "x") or
+            (self.new_board[1] == "x" and self.new_board[4] == "x" and self.new_board[7] == "x") or
+            (self.new_board[2] == "x" and self.new_board[5] == "x" and self.new_board[8] == "x") or
+            (self.new_board[0] == "x" and self.new_board[4] == "x" and self.new_board[8] == "x") or
+            (self.new_board[2] == "x" and self.new_board[4] == "x" and self.new_board[6] == "x")
+            ):
+            return True
+
+    def o_won(self):
+        if(
+            (self.new_board[0] == "o" and self.new_board[1] == "o" and self.new_board[2] == "o") or
+            (self.new_board[3] == "o" and self.new_board[4] == "o" and self.new_board[5] == "o") or
+            (self.new_board[6] == "o" and self.new_board[7] == "o" and self.new_board[8] == "o") or
+            (self.new_board[0] == "o" and self.new_board[3] == "o" and self.new_board[6] == "o") or
+            (self.new_board[1] == "o" and self.new_board[4] == "o" and self.new_board[7] == "o") or
+            (self.new_board[2] == "o" and self.new_board[5] == "o" and self.new_board[8] == "o") or
+            (self.new_board[0] == "o" and self.new_board[4] == "o" and self.new_board[8] == "o") or
+            (self.new_board[2] == "o" and self.new_board[4] == "o" and self.new_board[6] == "o")
+            ):
+            return True
--- test_game.py
+++ test_game.py
@@ -7,14 +7,13 @@ class TestGame(unittest.TestCase):
 
     def test_move(self):
         game = Game(board.empty_board)
-        new_board = game.make_move(0, "x")
-        new_board = game.make_move(2, "0")
-        self.assertEqual (new_board, ["x", " ", "0", " ", " ", " ", " ", " ", " "])
+        game.make_move(0, "x")
+        self.assertEqual (game.new_board[0], "x")
 
     def test_move_to_occupied_field(self):
         game = Game(board.empty_board)
         with pytest.raises(Exception) as excinfo:
-            game.make_move(2, "0")
+            game.make_move(0, "x")
         assert str(excinfo.value) == "Sorry, that field is already taken, choose another one!"
 
     def test_taken_field(self):
@@ -27,13 +26,26 @@ class TestGame(unittest.TestCase):
         empty = game.field_is_taken(1)
         self.assertEqual (empty, False)
 
-    def test_game_over(self):
+    def test_board_is_full(self):
         game = Game(["x", "x", "0", "x", "0", "x", "0", "0", "x"])
-        self.assertEqual (game.game_over(), True)
+        self.assertEqual (game.full_board(), True)
 
-    def test_game_not_over(self):
+    def test_board_isnt_full(self):
         game = Game([" ", "x", "0", "x", "0", "x", "0", "0", "x"])
-        self.assertEqual (game.game_over(), False)
+        self.assertEqual (game.full_board(), False)
+
+    def test_x_won(self):
+        game = Game(board.empty_board)
+        game.make_move(3, "x")
+        game.make_move(6, "x")
+        self.assertEqual (game.x_won(), True)
+
+    def test_0_won(self):
+        game = Game(board.empty_board)
+        game.make_move(2, "o")
+        game.make_move(5, "o")
+        game.make_move(8, "o")
+        self.assertEqual (game.o_won(), True)
 
 if __name__ == '__main__':
     unittest.main()
